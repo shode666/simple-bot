960d98a6d75e683bc11afb02ac2bb207564c3734
Bundit Rattanang <shode666@gmail.com>
2020-03-06 07:06:54 +0700

    add command to bot

--- bot.js
+++ bot.js
@@ -12,7 +12,12 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if(messages[msg.content]){
-    msg.reply(messages[msg.content])
+    msg.channel.send(messages[msg.content])
+  }else if(/^!set\s*"?([^" ]+)"?\s*"?([^"]*)"?\t*$/.test(msg.content)){
+    const g = msg.content.match(/^!set\s*"?([^" ]+)"?\s*"?([^"]*)"?\t*$/);
+    if(!!g && !!g[1]&&!!g[2])
+    messages[g[1]] = g[2];
+    msg.channel.send(`command registed say: ${g[1]} `)
   }
 });
 
