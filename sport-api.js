1748d9d187e3280240096846e931d80a1cc48d6e
shode666 <shode666@gmail.com>
2021-02-22 10:45:48 +0700

    new end match fetch style

--- sport-api.js
+++ sport-api.js
@@ -3,14 +3,15 @@ const fetch = require("./fetch");
 const moment = require('moment-timezone')
 const _ =require('lodash');
 
-let matchDailyCount = [];
+let __matchDailyCount = [];
+let __liveFixture = [];
 const MATCH_LENGTH = 120*60;
 module.exports = function(db){
   const __LEAGUES = process.env.RAPIDAPI_LEAGUE_ID||"";
 
   schedule.scheduleJob('0 0 * * *', ()=>{
     console.log("reset match day counter");
-    matchDailyCount = [];
+    __matchDailyCount = [];
   })
     let h=1,m=0,idx=1;
   __LEAGUES.split(",").filter(o=>!!o).forEach(leagueId=> {
@@ -75,13 +76,13 @@ function startDailyUpdate(db,leagueId){
   const yesterDay = today.subtract(1, 'days')
   const formatYesterDay = yesterDay.format("YYYY-MM-DD");
   console.log(`schedule start`,new Date())
+  __liveFixture = [];
   collection.get()
   .then(snapshot=>{
     const matches = _.filter(snapshot.val(),({event_timestamp})=>{
       return event_timestamp>=startTz && event_timestamp <=endTz;
     })
 
-    let quota = 80;
     let timeout = 0;
     const setDelay = () =>{
       timeout += 5 *1000;
@@ -96,11 +97,10 @@ function startDailyUpdate(db,leagueId){
     console.log('fixtures found ',matches.length)
     if(matches.length===0) return;
 
-    quota -= matches.length;
     const eventDates = matches.map(o=>o.event_timestamp);
     const minTime = _.min(eventDates);
     const maxTime = _.max(eventDates)+MATCH_LENGTH;
-    matchDailyCount.push({leagueId,minTime,maxTime});
+    __matchDailyCount.push({leagueId,minTime,maxTime});
     matches.forEach(match=>{
       // call Odd now
       setTimeout(()=>{
@@ -174,19 +174,17 @@ function updateDayFixture(db,leagueId,formatDate){
 }
 
 function liveScore(db){
-  if(!matchDailyCount.length) {
+  if(!__matchDailyCount.length) {
     console.log("no match for today")
     return;
   }
   console.log('Start finding Match Day')
-  const leagues = matchDailyCount.map(o=>o.leagueId).join("-");
+  const leagues = __matchDailyCount.map(o=>o.leagueId).join("-");
   console.log("Enter Live score for league_ids",leagues)
-  const minTime = Math.min(matchDailyCount.map(o=>o.minTime));
-  let maxTime = Math.max(matchDailyCount.map(o=>o.maxTime));
+  const minTime = _.min(__matchDailyCount.map(o=>o.minTime));
+  let maxTime = _.max(__matchDailyCount.map(o=>o.maxTime));
   const startTime = new Date(minTime*1000);
   const endTime = new Date(maxTime*1000);
-  const today = moment().clone().tz('Europe/London');
-  const formatToday = today.format("YYYY-MM-DD");
   const startM = moment(startTime)
   const endM = moment(endTime)
   const minuteDiff = endM.diff(startM,'minutes')
@@ -195,14 +193,6 @@ function liveScore(db){
   schedule.scheduleJob({ start: startTime, end: endTime, rule: `*/${frequency} * * * *`}, ()=>{
     fetchLiveScore(db,leagues);
   });
-
-  matchDailyCount.forEach(({leagueId})=>{
-    maxTime+=5
-    schedule.scheduleJob(new Date(maxTime*1000),()=>{
-      console.log("last update fixture for",leagueId,"date",formatToday)
-      updateDayFixture(db,leagueId,formatToday);
-    });
-  })
 }
 
 function fetchLiveScore(db,leagues){
@@ -214,8 +204,10 @@ function fetchLiveScore(db,leagues){
     if(!fixtures||!fixtures.length)return;
     console.log(`fetched live fixture ${leagues} fetching fixtures Collection`)
     const fixtureCollectionRef = db.ref(`football-league/fixtures`);
+    const newLiveFix = []
     fixtures.forEach(fixure=>{
       const {fixture_id, league_id} = fixure;
+      newLiveFix.push(fixture_id)
       const fixureCollection = fixtureCollectionRef.child(String(league_id)).child(String(fixture_id));
       if(fixture_id){
         console.log(`batch update livescore ${league_id} fixure_id ${fixture_id}`)
@@ -223,8 +215,36 @@ function fetchLiveScore(db,leagues){
       }
     });
     console.log('commit batch fixtures')
+    // finding game ended
+    console.log('-- finding game ended')
+    const endFix = _.differenceWith(__liveFixture, newLiveFix, _.isEqual);
+
+    fetchEndedGame(db,endFix);
+
+    __liveFixture = newLiveFix;
     return;
   }).catch( (error) => {
     console.error(error);
+  });
+}
+
+function fetchEndedGame(db,endFix){
+  if(!endFix.length) return console.log('no ended');
+
+  console.log('-- // ended = ',endFix)
+  const fixtureCollectionRef = db.ref(`football-league/fixtures`);
+  endFix.forEach(fixtureId=>{
+    fetch(`https://${process.env.RAPIDAPI_HOST}/v2/fixtures/id/${fixtureId}`,{timezone: 'Europe/London'})
+    .then(({fixtures})=>{
+      if(!fixtures||!fixtures.length)return;
+        console.log(`fetched end fixture ${fixtureId}`)
+        const fixure = fixtures[0];
+        const {fixture_id, league_id} = fixure;
+        const fixureCollection = fixtureCollectionRef.child(String(league_id)).child(String(fixture_id));
+        if(fixture_id){
+          console.log(`batch update end game FT ${league_id} fixure_id ${fixture_id}`)
+          fixureCollection.update(fixure);
+        }
+    });
   });
 }
